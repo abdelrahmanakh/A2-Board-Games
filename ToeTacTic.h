3d14be5892029ccb512c757add7e7c1cd104254d
abdelrahmanakh <akhteebabdo@gmail.com>
2024-12-15 18:51:05 +0200

    comments on my 2 Header files

--- ToeTacTic.h
+++ ToeTacTic.h
@@ -13,34 +13,41 @@
 using namespace std;
 
 template<typename T>
-class ToeTacTicBoard: public Board<T>{
+class ToeTacTicBoard : public Board<T> {
 public:
-    Player<T>* winner;//for the GameManager run method
-    Player<T>* players[2];//for the GameManager run method
-    ToeTacTicBoard(Player<T>* player1, Player<T>* player2);
-    bool isloser = false; //for the GameManager run method
+    Player<T> *winner;//to get the winner
+    Player<T> *players[2];
+
+    ToeTacTicBoard(Player<T> *player1, Player<T> *player2);
+
+    bool isloser = false; //boolean value to know when someone loses
     bool update_board(int x, int y, T symbol);
+
     void display_board();
+
     bool is_win();
+
     bool is_draw();
+
     bool game_is_over();
 };
 
 template<typename T>
-class ToeTacTicPlayer: public Player<T>{
+class ToeTacTicPlayer : public Player<T> {
 public:
     ToeTacTicPlayer(string name, T symbol);
-    void getmove(int& x, int& y);
+
+    void getmove(int &x, int &y);
 };
 
 template<typename T>
-class ToeTacTicRandomPlayer: public RandomPlayer<T>{
+class ToeTacTicRandomPlayer : public RandomPlayer<T> {
 public:
     ToeTacTicRandomPlayer(T symbol);
-    void getmove(int& x, int& y);
+
+    void getmove(int &x, int &y);
 
 };
-
 
 
 //-------------------------------------
@@ -48,14 +55,14 @@ public:
 //-------------------------------------
 //ToeTacTicBoard constructor
 template<typename T>
-ToeTacTicBoard<T>::ToeTacTicBoard(Player<T>* player1, Player<T>* player2){
+ToeTacTicBoard<T>::ToeTacTicBoard(Player<T> *player1, Player<T> *player2) {
     this->rows = 3;
     this->columns = 3;
-    this->board = new char*[this->rows];
+    this->board = new char *[this->rows];
     for (int i = 0; i < this->rows; ++i) {
-        this->board[i] = new char[this->columns];
+        this->board[i] = new char[this->columns];//each element in the row is a pointer to another array
         for (int j = 0; j < this->columns; ++j) {
-            this->board[i][j] =  0;
+            this->board[i][j] = 0;
         }
     }
     this->n_moves = 0;
@@ -66,9 +73,10 @@ ToeTacTicBoard<T>::ToeTacTicBoard(Player<T>* player1, Player<T>* player2){
 //Update Board Function
 template<typename T>
 bool ToeTacTicBoard<T>::update_board(int x, int y, T symbol) {
-    if(isloser)
+    if (isloser)//if there is a loser don't update the board and skip
         return true;
-    if((x>=0 && x<this->rows && y>=0 && y<this->columns && this->board[x][y] == 0)){
+    //check the boundaries and if the cell is empty or not before making the move
+    if ((x >= 0 && x < this->rows && y >= 0 && y < this->columns && this->board[x][y] == 0)) {
         this->board[x][y] = symbol;
         this->n_moves++;
         return true;
@@ -93,7 +101,8 @@ void ToeTacTicBoard<T>::display_board() {
 //Is draw
 template<typename T>
 bool ToeTacTicBoard<T>::is_draw() {
-    if (this->n_moves==9&&!is_win()){
+    //checks if the game is over and there is no loser
+    if (this->n_moves == 9 && isloser) {
         return true;
     }
     return false;
@@ -102,52 +111,55 @@ bool ToeTacTicBoard<T>::is_draw() {
 //Is win
 template<typename T>
 bool ToeTacTicBoard<T>::is_win() {
-    if((this->board[1][1] == this->board[0][0] && this->board[1][1] == this->board[2][2] && this->board[1][1]!=0)||
-       (this->board[1][1] == this->board[0][2] && this->board[1][1] == this->board[2][0] && this->board[1][1]!=0)){
-        if(this->board[1][1]==this->players[0]->getsymbol()){
-            this->winner= this->players[1];
-        }
-        else
-            this->winner = this->players[0];
-        this->isloser= true;
+    //check if there is a diagonal of one symbol
+    if ((this->board[1][1] == this->board[0][0] && this->board[1][1] == this->board[2][2] && this->board[1][1] != 0) ||
+        (this->board[1][1] == this->board[0][2] && this->board[1][1] == this->board[2][0] && this->board[1][1] != 0)) {
+        if (this->board[1][1] == this->players[0]->getsymbol()) {
+            this->winner = this->players[1];//if the diagonal has symbol of player 0 the winner is player 1
+        } else
+            this->winner = this->players[0];//if the diagonal has symbol of player 1 the winner is player 0
+        this->isloser = true; //As there is a diagonal of symbols there is a loser
         return false;
     }
     for (int i = 0; i < this->rows; ++i) {
-        T cell = this->board[i][0];
+        T cell = this->board[i][0];//take the first cell in each row
         if (cell == 0)
             continue;
-        int rc = 0,cc=0;
+        int rc = 0, cc = 0;//number of row consecutive and col consecutive
+        //loop through each col in the row
         for (int j = 1; j < this->columns; ++j) {
-            if(cell == this->board[i][j]){
+            //if there is another cell in the row with the same symbol increase rc by 1
+            if (cell == this->board[i][j]) {
                 rc++;
-            } else{
-                rc=0;
+            } else {
+                rc = 0;
             }
-            if(rc==2) {
+            // if rc equals 2 then there is 3 same symbols in a raw
+            if (rc == 2) {
                 this->isloser = true;
-                if(cell==this->players[0]->getsymbol()){
-                    this->winner= this->players[1];
-                }
-                else
+                if (cell == this->players[0]->getsymbol()) {
+                    this->winner = this->players[1];
+                } else
                     this->winner = this->players[0];
                 return false;
             }
         }
-        cell = this->board[0][i];
+        cell = this->board[0][i];//take first cell in each col
         if (cell == 0)
             continue;
         for (int j = 1; j < this->rows; ++j) {
-            if(cell == this->board[j][i]){
+            //if there is another cell in the col with the same symbol increase cc by 1
+            if (cell == this->board[j][i]) {
                 cc++;
-            } else{
-                cc=0;
+            } else {
+                cc = 0;
             }
-            if(cc==2) {
+            // if rc equals 2 then there is 3 same symbols in a col
+            if (cc == 2) {
                 this->isloser = true;
-                if(cell==this->players[0]->getsymbol()){
-                    this->winner= this->players[1];
-                }
-                else
+                if (cell == this->players[0]->getsymbol()) {
+                    this->winner = this->players[1];
+                } else
                     this->winner = this->players[0];
                 return false;
             }
@@ -159,7 +171,8 @@ bool ToeTacTicBoard<T>::is_win() {
 //game_is_over
 template<typename T>
 bool ToeTacTicBoard<T>::game_is_over() {
-    if (is_draw() || isloser){
+    //if 9 cells are used or there is a loser the game is over
+    if (this->n_moves == 9 || isloser) {
         return true;
     }
     return false;
@@ -171,11 +184,12 @@ ToeTacTicPlayer<T>::ToeTacTicPlayer(std::string name, T symbol): Player<T>(name,
 
 //Get move
 template<typename T>
-void  ToeTacTicPlayer<T>::getmove(int &x, int &y) {
-    char i,j;
+void ToeTacTicPlayer<T>::getmove(int &x, int &y) {
+    char i, j;
     cout << "Please enter the coordinates of the square that you want (0 to 2)\n";
+    //takes X and ensures it is within the boundaries and a num
     while (true) {
-        cout<<"x:";
+        cout << "x:";
         cin >> i;
         if (i >= '0' && i <= '2') {
             x = i - '0';
@@ -184,8 +198,9 @@ void  ToeTacTicPlayer<T>::getmove(int &x, int &y) {
             cout << "invalid input\n";
         }
     }
+    //takes Y and ensures it is within the boundaries and a num
     while (true) {
-        cout<<"y:";
+        cout << "y:";
         cin >> j;
         if (j >= '0' && j <= '2') {
             y = j - '0';
@@ -196,16 +211,17 @@ void  ToeTacTicPlayer<T>::getmove(int &x, int &y) {
     }
 }
 
-//:TicTacToe5x5RandomPlayer constructor
+//ToeTacTicRandomPlayer constructor
 template<typename T>
-ToeTacTicRandomPlayer<T>::ToeTacTicRandomPlayer(T symbol):RandomPlayer<T>(symbol){
+ToeTacTicRandomPlayer<T>::ToeTacTicRandomPlayer(T symbol):RandomPlayer<T>(symbol) {
     this->dimension = 5;
     this->name = "Random Computer Player";
     srand(static_cast<unsigned int>(time(0)));
 }
 
-template <typename T>
-void ToeTacTicRandomPlayer<T>::getmove(int& x, int& y) {
+//ToeTacTicRandomPlayer getmove function
+template<typename T>
+void ToeTacTicRandomPlayer<T>::getmove(int &x, int &y) {
     x = rand() % this->dimension;
     y = rand() % this->dimension;
 }
